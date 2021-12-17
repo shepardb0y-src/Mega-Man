d5c1d660991dd365aa94208abba2adfdce57de86
shepardb0y-src <60862129+shepardb0y-src@users.noreply.github.com>
2021-12-17 14:54:51 -0500

    got a sprite to appear using all 9 of draw image paramaters

--- script.js
+++ script.js
@@ -10,20 +10,36 @@ const player = {
   y: 0,
   framX: 0,
   frameY: 0,
-  height: 22.4,
-  width: 26.8,
+  height: 100,
+  width: 53,
   speed: 9,
   moving: false,
 };
 
 const playerSpriteImage = new Image();
-playerSpriteImage.src = ".../images/megaman.png";
+playerSpriteImage.src = "../images/megaman.png";
 const background = new Image();
 background.src = "../images/boss-zone.png";
 console.log(background);
 
+function drawSprite(img, sx, sy, sw, sh, dx, dy, dw, dh) {
+  ctx.drawImage(img, sx, sy, sw, sh, dx, dy, dw, dh);
+}
+
 function animate() {
+  // ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+  drawSprite(
+    playerSpriteImage,
+    0,
+    0,
+    player.width,
+    player.height,
+    200,
+    150,
+    player.width,
+    player.height
+  );
   requestAnimationFrame(animate);
 }
 animate();
