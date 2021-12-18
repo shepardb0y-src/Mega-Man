a0e1efa2f2663f754d720c3ee503c303444e2df3
shepardb0y-src <60862129+shepardb0y-src@users.noreply.github.com>
2021-12-17 21:47:15 -0500

    got megman to move utilixzing the key property

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ canvas.height = 500;
 const keys = [];
 
 const player = {
-  x: 0,
-  y: 0,
+  x: 200,
+  y: 200,
   framX: 0,
   frameY: 0,
-  height: 30,
+  height: 28,
   width: 28,
   speed: 9,
   moving: false,
@@ -35,11 +35,85 @@ function animate() {
     0,
     player.width,
     player.height,
-    200,
-    150,
+    player.x,
+    player.y,
     player.width,
     player.height
   );
+  // moveSprite();
   requestAnimationFrame(animate);
 }
 animate();
+
+// window.addEventListener("keydown", function (e) {
+//   keys[e.keys] = true;
+//   console.log(e.keys);
+// });
+
+// window.addEventListener("keyup", function (e) {
+//   delete keys[e.keys];
+// });
+
+window.addEventListener(
+  "keydown",
+  function (event) {
+    if (event.defaultPrevented) {
+      return; // Do nothing if the event was already processed
+    }
+
+    switch (event.key) {
+      case "Down": // IE/Edge specific value
+      case "ArrowDown":
+        console.log("down");
+        moveSpritedown();
+        // Do something for "down arrow" key press.
+        break;
+      case "Up": // IE/Edge specific value
+      case "ArrowUp":
+        console.log("up");
+        moveSpriteup();
+        // Do something for "up arrow" key press.
+        break;
+      case "Left": // IE/Edge specific value
+      case "ArrowLeft":
+        console.log("left");
+        moveSpriteleft();
+        // Do something for "left arrow" key press.
+        break;
+      case "Right": // IE/Edge specific value
+      case "ArrowRight":
+        console.log("right");
+        moveSpriteright();
+        // Do something for "right arrow" key press.
+        break;
+      case "Enter":
+        // Do something for "enter" or "return" key press.
+        break;
+      case "Esc": // IE/Edge specific value
+      case "Escape":
+        // Do something for "esc" key press.
+        break;
+      default:
+        return; // Quit when this doesn't handle the key event.
+    }
+
+    // Cancel the default action to avoid it being handled twice
+    event.preventDefault();
+  },
+  true
+);
+
+function moveSpriteup(param) {
+  player.y += player.speed;
+}
+
+function moveSpritedown(param) {
+  player.y -= player.speed;
+}
+
+function moveSpriteleft(param) {
+  player.x -= player.speed;
+}
+function moveSpriteright(param) {
+  player.x += player.speed;
+}
