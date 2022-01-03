c4b271b5e3be333bdd66c66ad2de9fdc414e8f8d
shepardb0y-src <60862129+shepardb0y-src@users.noreply.github.com>
2022-01-03 13:48:39 -0500

    complete read me

--- script.js
+++ script.js
@@ -4,10 +4,8 @@ console.log(canvas);
 canvas.width = 800;
 canvas.height = 500;
 const start = document.querySelector("#StartButton");
-
 const game = {
   start: () => {
-    // console.log('We are starting a game!')
     canvas.classList.add("hide");
     canvas.classList.remove("hide");
   },
@@ -20,7 +18,6 @@ const player = {
   y: 400,
   frameY: 8.7,
   frameX: 0.5,
-  /// default about .2-.5 for width of sprite defualt for height 1.8 or 13 or count the rows on the image
   height: 100,
   width: 100,
   speed: 9,
@@ -35,7 +32,6 @@ const enemy = {
   y: 200,
   frameY: 0,
   frameX: 0,
-  /// default about .2-.5 for width of sprite defualt for height 1.8 or 13 or count the rows on the image
   height: 100,
   width: 80,
   speed: 9,
@@ -64,7 +60,7 @@ class Healthbar {
   updateHealthplayer(val) {
     health = val;
     this.w = this.w - 2;
-    // console.log(this.w);
+
     health = this.w;
     console.log(health);
     if (health <= 0) {
@@ -81,9 +77,6 @@ class Healthbar {
     }
   }
   restartplayer() {
-    // if (player.w === 0 && player.missle) {
-    //   player.alive = false;
-    // }
     if (enemy.alive && this.w === 0) {
       newhealth;
       enemyhealth = newhealth;
@@ -94,37 +87,9 @@ class Healthbar {
       console.log(game.playerround);
       location.reload();
       alert("enemy wins");
-      // console.log(`${this.w} here `);
     } else if (enemy.alive && player.alive) {
       alert("both dead");
     }
-    // if (!player.alive && !enemy.alive) {
-    //   // location.reload();
-    //   ctx.font = "200px comic Sans";
-    //   ctx.fillStyle = "gold";
-    //   ctx.fillText("game over", 10, 150);
-    //   // alert(" game over");
-    //   console.log("winn conditoion");
-    // }
-    // if (player.alive && !enemy.alive) {
-    //   alert("game continues ");
-    //   console.log("winn conditoion");
-    // }
-    // if (!enemy.alive && this.w === 0 && !game.enemyround) {
-    //   newhealth;
-    //   health = newhealth;
-    //   this.w = newhealth;
-    //   game.enemyround = 1;
-    //   game.playerround = 1;
-    //   player.alive = false;
-    //   console.log(game.enemyround);
-    //   alert("endround 2");
-    // }
-
-    // if (player.alive && enemy.alive) {
-    //   alert("player wins");
-    //   console.log("winn conditoion");
-    // }
   }
   restartenemy() {
     if (player.alive && this.w === 0) {
@@ -135,7 +100,6 @@ class Healthbar {
       enemy.alive = false;
       console.log(enemy.alive);
       console.log(game.playerround);
-
       ctx.font = "200px comic Sans";
       ctx.fillStyle = "orange";
       ctx.fillText("game over", 10, 300);
@@ -185,7 +149,6 @@ function drawSprite(img, sx, sy, sw, sh, dx, dy, dw, dh) {
 }
 
 function animate() {
-  // ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
   drawSprite(
     playerSpriteImage,
@@ -242,48 +205,38 @@ window.addEventListener(
   "keydown",
   function (event) {
     if (event.defaultPrevented) {
-      return; // Do nothing if the event was already processed
+      return;
     }
 
     switch (event.key) {
-      case "Down": // IE/Edge specific value
+      case "Down":
       case "ArrowDown":
         console.log("down");
         moveSpritedown();
-        // Do something for "down arrow" key press.
+
         break;
-      case "Up": // IE/Edge specific value
+      case "Up":
       case "ArrowUp":
         console.log("up");
         moveSpriteup();
-        // Do something for "up arrow" key press.
+
         break;
-      case "Left": // IE/Edge specific value
+      case "Left":
       case "ArrowLeft":
         console.log("left");
         moveSpriteleft();
-        // Do something for "left arrow" key press.
+
         break;
-      case "Right": // IE/Edge specific value
+      case "Right":
       case "ArrowRight":
         console.log("right");
         moveSpriteright();
-        // Do something for "right arrow" key press.
-        break;
-      case "Enter":
-        // Do something for "enter" or "return" key press.
-        // const missle = new Missles(player.x, player.y, 30, "blue", null);
-        // missle.draw();
-        // console.log(missle.draw());
-        // animatemissle();
-        // console.log(animatemissle());
-        // console.log(animate.missle);
-        // attaack();
 
         break;
-      case "Esc": // IE/Edge specific value
+      case "Enter":
+        break;
+      case "Esc":
       case "Escape":
-        // Do something for "esc" key press.
         player.missle.push({
           width: 20,
           height: 20,
@@ -298,18 +251,13 @@ window.addEventListener(
           } else {
             player.missle[i].x += player.speed;
           }
-
-          // player.missle[i].x = player.x;
-          // player.missle[i].y = player.y + player.missle[i].y;
-          // console.log(player.missle[i].x);
         }
 
         break;
       default:
-        return; // Quit when this doesn't handle the key event.
+        return;
     }
 
-    // Cancel the default action to avoid it being handled twice
     event.preventDefault();
   },
   true
@@ -365,7 +313,6 @@ function moveSpriteright(param) {
   }
 }
 
-/// Collision detection
 function collisionplayer(a, b) {
   if (
     a.x < b.x + b.width &&
@@ -378,7 +325,6 @@ function collisionplayer(a, b) {
     enemyHealthbar.updateHealthenemy();
   } else {
     console.log("no collision");
-    // playerHealthbar.updateHealthplayer();
   }
 }
 
@@ -391,13 +337,9 @@ function collisionenemy(a, b) {
   ) {
     console.log("collision");
     playerHealthbar.updateHealthplayer();
-    // enemyHealthbar.updateHealthenemy();
   } else {
     console.log("no collision");
-
-    // enemyHealthbar.updateHealthenemy();
   }
 }
-// game.win();
 
 window.onload = setInterval(animate, 100 * 1000);
